0524f3838f1306c1b2f95cd1498a0b9c8b78d4bf
Zade Viggers <zade.viggers@gmail.com>
2022-04-22 17:06:50 +1200

    new icon & material 3
    Signed-off-by: Zade Viggers <zade.viggers@gmail.com>

--- app/src/main/java/net/viggers/zade/wallpaper/MainActivity.kt
+++ app/src/main/java/net/viggers/zade/wallpaper/MainActivity.kt
@@ -1,20 +1,20 @@
 package net.viggers.zade.wallpaper
 
-import android.app.Activity
-import android.app.ActivityManager
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import net.viggers.zade.wallpaper.R
 import android.content.Intent
 import android.app.WallpaperManager
 import android.content.ComponentName
 import android.view.View
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
     }
 
+    // DO NOT REMOVE THIS PARAMETER, EVEN THOUGH IT'S NOT USED
+    // Removing it makes the app crash when the button is clicked
     fun onInstallClick(view: View?) {
         val intent = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
         intent.putExtra(
@@ -24,6 +24,8 @@ class MainActivity : Activity() {
         startActivity(intent)
     }
 
+    // DO NOT REMOVE THIS PARAMETER, EVEN THOUGH IT'S NOT USED
+    // Removing it makes the app crash when the button is clicked
     fun onPrefsClick(view: View?) {
         val intent = Intent(this, PreferencesActivity::class.java)
         startActivity(intent)
