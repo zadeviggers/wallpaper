59cb737a509d48cc26a79586383a22d3a3bbfd8f
Zade Viggers <zade@viggers.net>
2022-12-17 16:07:37 +1300

    Add link to downloads page

--- app/src/main/java/net/viggers/zade/wallpaper/MainActivity.kt
+++ app/src/main/java/net/viggers/zade/wallpaper/MainActivity.kt
@@ -3,6 +3,7 @@ package net.viggers.zade.wallpaper
 import android.app.WallpaperManager
 import android.content.ComponentName
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         val installButton = findViewById<Button>(R.id.installButton)
         val prefsButton = findViewById<Button>(R.id.prefsButton)
         val clearShapesButton = findViewById<Button>(R.id.clearShapesButton)
+        val downloadsPageButton = findViewById<Button>(R.id.downloadsPageButton)
 
         val context = this
         installButton.setOnClickListener {
@@ -35,5 +37,14 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(getString(R.string.action_remove_all_shapes))
             sendBroadcast(intent)
         }
+        downloadsPageButton.setOnClickListener {
+            goToUrl("https://github.com/zadeviggers/wallpaper/releases")
+        }
     }
+    private fun goToUrl(url: String) {
+        val uriUrl: Uri = Uri.parse(url)
+        val launchBrowser = Intent(Intent.ACTION_VIEW, uriUrl)
+        startActivity(launchBrowser)
+    }
+
 }
